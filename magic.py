fe211204248e3df1d70765dab1658b8e272da8de
Alexandre Muniz <aom@puzzlezapper.com>
2021-03-21 01:33:08 -0700

    use ndlist

--- magic.py
+++ magic.py
@@ -2,54 +2,53 @@ from operator import mul
 from functools import reduce
 from ortools.sat.python import cp_model
 from itertools import product
+from more_itertools import collapse
 from copy import copy
-
-class MagicPrinter(cp_model.CpSolverSolutionCallback):
-    def __init__(self, board):
-        cp_model.CpSolverSolutionCallback.__init__(self)
-        self.board = board
-    
-    def OnSolutionCallback(self):
-        print(self.board)
-        for cell in self.board.values():
-            print(self.Value(cell))
-            print()
-        print()
+from ndlist import ndlist
+from solprinters import IntPrinter
 
 class MagicModel(cp_model.CpModel):
-    def __init__(self, board_dims, min_cell=1, **kwargs):
-        self.board_dims = board_dims
+    def __init__(self, dims, min_cell=1, **kwargs):
+        self.dims = dims
         self.min_cell = min_cell
+        cell_cnt = reduce(mul, self.dims, 1)
+        self.max_cell = self.min_cell + cell_cnt - 1
         super().__init__()
         self.setup()
 
     def setup(self):
-        cell_cnt = reduce(mul, self.board_dims, 1)
-        max_cell = self.min_cell + cell_cnt - 1
-        twice_mean = self.min_cell + max_cell
+
+        twice_mean = self.min_cell + self.max_cell
         # if twice_mean is odd, and a board_dim is odd, it's not gonna work
-        magic_sums = [twice_mean * board_dim // 2 for board_dim in self.board_dims]
-        print(magic_sums)
+        self.magic_sums = [twice_mean * board_dim // 2 for board_dim in self.dims]
+        print("magic sums", self.magic_sums)
         lines = {}
-        self.board = {}
-        # model = cp_model.CpModel()
-        for cell in product(*[range(dim) for dim in self.board_dims]):
-            new_int_var = self.NewIntVar(self.min_cell, max_cell, repr(cell))
-            self.board[cell] = new_int_var
-            for i in range(len(self.board_dims)):
-                line_key = list(cell)
-                line_key[i] = None
-                line_key = tuple(line_key)
-                line_val = lines.get(line_key)
-                if not line_val:
-                    line_val = []
-                    lines[line_key] = line_val
-                line_val.append(new_int_var)
-        for line_item in lines.items():
-            dim_num = line_item[0].index(None)
-            self.Add(sum(line_item[1]) == magic_sums[dim_num])
-        self.AddAllDifferent(self.board.values())
-        self.solution_printer = MagicPrinter(self.board)
+        self.board = ndlist.empty(self.dims)
+        # ranges = [range(dim) for dim in self.dims]
+        for entry in product(*self.board.ranges):
+            new_int_var = self.NewIntVar(self.min_cell, self.max_cell, repr(entry))
+            self.board[entry] = new_int_var
+        # for cell in product(*[range(dim) for dim in self.dims]):
+
+        #     for i in range(len(self.dims)):
+        #         line_key = list(cell)
+        #         line_key[i] = None
+        #         line_key = tuple(line_key)
+        #         line_val = lines.get(line_key)
+        #         if not line_val:
+        #             line_val = []
+        #             lines[line_key] = line_val
+        #         line_val.append(new_int_var)
+        # for line_item in lines.items():
+        #     dim_num = line_item[0].index(None)
+        #     self.Add(sum(line_item[1]) == self.magic_sums[dim_num])
+        for dim_num, lines in enumerate(self.board.lines()):
+            for line in lines:
+                # print(line)
+                self.Add(sum(line) == self.magic_sums[dim_num])
+        # print(collapse(self.board))
+        self.AddAllDifferent(collapse(self.board))
+        self.solution_printer = IntPrinter(self.board)
 
     def solve(self, **kwargs):
         show_solutions = kwargs.get('show_solutions')
