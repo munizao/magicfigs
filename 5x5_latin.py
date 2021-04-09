0b6d850cabd8beaa54cc9bea1454166ff31fefcb
Alexandre Muniz <aom@puzzlezapper.com>
2021-04-08 21:28:00 -0700

    got metalatin working

--- 5x5_latin.py
+++ 5x5_latin.py
@@ -1,4 +1,6 @@
+from metaify import metaify
 from latin import LatinModel
 
-model = LatinModel((5, 5))
+model = LatinModel((5, 5), min_cell=0)
+metaify(model, (2, 2), kitty_corners=True, puddleless=True, single_form=True, diagonals=True)
 model.solve()
