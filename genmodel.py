45f19edd4ca9f6da694b53f3ccf1ff3f90b321fe
Alexandre Muniz <aom@puzzlezapper.com>
2021-04-08 21:27:08 -0700

    Moved some stuff from magic

--- genmodel.py
+++ genmodel.py
@@ -1,3 +1,5 @@
+from operator import mul
+from functools import reduce
 from itertools import product
 from more_itertools import all_equal
 from copy import copy
@@ -8,9 +10,16 @@ class GenModel(cp_model.CpModel):
     def __init__(self, dims, min_cell=1, **kwargs):
         self.dims = dims
         self.min_cell = min_cell
-        self.max_cell = min_cell + max(dims) - 1
+        self.cell_cnt = reduce(mul, self.dims, 1)
+
         super().__init__()
+
+        twice_mean = self.min_cell + self.max_cell
+        # if twice_mean is odd, and a board_dim is odd, it's not gonna work
+        self.magic_sums = [twice_mean * dim // 2 for dim in self.dims]
         self.board = ndlist.empty(self.dims)
+        self.board.total = twice_mean * self.cell_cnt // 2
+
         self.setup()
     
     def setup(self):
@@ -23,7 +32,7 @@ class GenModel(cp_model.CpModel):
         origin = [0 for _ in self.dims]
         for i in range(1, 2 ** len(self.dims)):
             corner = [(self.dims[n] - 1) * ((i >> n) % 2) for n in range(len(self.dims))]
-            self.Add(self.board[origin] < self.board[corner])
+            self.Add(self.board[origin] <= self.board[corner])
         if all_equal(self.dims):
             for i, dim in enumerate(self.dims):
                 corner = copy(origin)
@@ -31,7 +40,7 @@ class GenModel(cp_model.CpModel):
                 if i == 0:
                     first_corner = corner
                     continue
-                self.Add(self.board[first_corner] < self.board[corner])
+                self.Add(self.board[first_corner] <= self.board[corner])
     
     def solve(self, **kwargs):
         # show_solutions = kwargs.get('show_solutions')
