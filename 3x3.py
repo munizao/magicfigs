d3d055ca9710f874f53c25280e3ba357e8cc2fac
Alexandre Muniz <aom@puzzlezapper.com>
2021-04-18 15:33:58 -0700

    moved more examples

--- 3x3.py
+++
@@ -1,6 +0,0 @@
-from metaify import metaify
-from magic import MagicModel
-
-model = MagicModel((3, 3), diagonals=True)
-metaify(model, (3, 3), diagonals=True, kitty_corners=True, puddleless=True, single_form=True)
-model.solve()
---
+++ examples/4x4_0index.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append('./')
+
+from metaify import metaify
+from boards.magic import MagicBoard
+
+# Zero-indexed 4×4 magic squares can be used as a basis for sparse squares where 
+# each number in the original square corresponds to a 3×5 cell in the corresponding 
+# sparse square.
+
+board = MagicBoard((4, 4), min_cell=0, diagonals=True)
+metaify(board, (3, 5), kitty_corners=True, puddleless=True, single_form=True)
+board.model.solve()
