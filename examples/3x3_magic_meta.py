e9ac1038ec3316b73b0dadd77f1d04c67f7d32a0
Alexandre Muniz <aom@puzzlezapper.com>
2021-05-07 18:12:33 -0700

    moved file to disambiguate 3x3 variants.

--- examples/3x3_magic_meta.py
+++ examples/3x3_magic_meta.py
@@ -2,8 +2,10 @@ import sys
 sys.path.append('./')
 
 from metaify import metaify
+from magicify import magicify
 from boards.magic import MagicBoard
 
 board = MagicBoard((3, 3), diagonals=True)
 metaify(board, (3, 3), diagonals=True, kitty_corners=True, puddleless=True, single_form=True)
+magicify(board)
 board.model.solve()
